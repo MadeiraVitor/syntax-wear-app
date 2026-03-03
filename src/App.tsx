1a2f67a7b43e76999a1084c2bae91afd414ac573
Vitor Madeira <vitorsoutom@hotmail.com>
2026-03-03 11:47:33 -0300

    feat: add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-
+import { Header } from "./components/Header"
 
 function App() {
 
   return (
     <>
-      <p className="text-error font-semibold">Hello, Syntax Wear App!</p>
+      <Header />
+      
     </>
   )
 }
