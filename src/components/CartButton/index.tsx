ee4f003d3b921b92e4aa7f8cd7cb8abeb71fa3f6
Vitor Madeira <vitorsoutom@hotmail.com>
2026-03-19 11:18:39 -0300

    fix: corrects the product quantity icon in the cart

--- src/components/CartButton/index.tsx
+++ src/components/CartButton/index.tsx
@@ -7,14 +7,14 @@ interface CartButtonProps {
 }
 
 export const CartButton = ({ onClick }: CartButtonProps) => {
-  const { cart } = useContext(CartContext);
+  const { cartItemsCount } = useContext(CartContext);
 
   return (
     <button className="relative cursor-pointer flex items-center" onClick={onClick}>
       <img src={IconCart} alt="Ícone Carrinho" />
-      {cart.length > 0 && (
+      {cartItemsCount > 0 && (
         <span className="absolute -top-2 -right-2 flex h-5 min-w-5 items-center justify-center rounded-full bg-error px-1 text-[10px] font-semibold text-white">
-          {cart.length}
+          {cartItemsCount}
         </span>
       )}
     </button>
